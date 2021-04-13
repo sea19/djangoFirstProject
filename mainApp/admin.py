8354fec6dec8529f33dfd262dab8e66b984f3837
Evgenia <evgenia.ugra@gmail.com>
2021-04-13 21:36:10 +0500

    Добавлены поиск и фильтрация, подсчет скидки для постоянных клиентов

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -4,14 +4,20 @@ from . import models
 
 @admin.register(models.HaircutModel)
 class HaircutAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'gender', 'price')
+    search_fields = ('name', 'gender', 'price')
+    list_filter = ('gender',)
 
 
 @admin.register(models.ClientModel)
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'patronymic', 'phone', 'email', 'status')
+    search_fields = ('last_name', 'first_name', 'patronymic', 'phone', 'email')
+    list_filter = ('status',)
 
 
 @admin.register(models.WorkModel)
 class WorkAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('date_time', 'haircut', 'client', 'master', 'final_cost')
+    search_fields = ('date_time', 'haircut', 'client', 'master', 'final_cost')
+
--- mainApp/models.py
+++ mainApp/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import AbstractUser
 
 User = get_user_model()
 
 
 class HaircutModel(models.Model):
     CHECKLIST_OPTIONS = (
-        (True, 'женская'),
-        (False, 'мужская'),
+        ('f', 'женская'),
+        ('m', 'мужская'),
     )
 
-    name = models.CharField(null=False, max_length=256, verbose_name='Название стрижки')
-    gender = models.BooleanField(null=False, choices=CHECKLIST_OPTIONS, verbose_name='Пол')
-    price = models.DecimalField(null=False, max_digits=9, decimal_places=2, verbose_name='Цена')
+    name = models.CharField(max_length=256, verbose_name='Название стрижки')
+    gender = models.CharField(max_length=1, choices=CHECKLIST_OPTIONS, verbose_name='Пол')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
 
     def __str__(self):
         return self.name
@@ -34,7 +35,7 @@ class ClientModel(models.Model):
 
     def __str__(self):
         return '{} {} {}'.format(self.last_name, self.first_name,
-                                 self.patronymic if self.patronymic is not None else '')
+                                 self.patronymic or '')
 
     class Meta:
         verbose_name = 'Клиент'
@@ -46,13 +47,18 @@ class WorkModel(models.Model):
     haircut = models.ForeignKey('HaircutModel', null=True, verbose_name='Стрижка', on_delete=models.DO_NOTHING)
     client = models.ForeignKey('ClientModel', null=True, verbose_name='Клиент', on_delete=models.DO_NOTHING)
     master = models.ForeignKey(User, null=True, verbose_name='Мастер', on_delete=models.DO_NOTHING)
-    date_time = models.DateTimeField(null=True, verbose_name='Дата и время')
+    date_time = models.DateTimeField(verbose_name='Дата и время')
+    final_cost = models.DecimalField(default=0, max_digits=9, decimal_places=2, verbose_name='Общая стоимость')
 
     def __str__(self):
-        return 'Стрижка: {}, клиент: {} {} {}, мастер: {} {}, дата и время: {}'\
-            .format(self.haircut.name, self.client.last_name, self.client.first_name,
-                    self.client.patronymic if self.client.patronymic is not None else '', self.master.last_name,
-                    self.master.first_name, self.date_time)
+        return 'Работа'
+
+    def save(self):
+        if self.client.status:
+            self.final_cost = self.haircut.price-self.haircut.price*5/100
+        else:
+            self.final_cost = self.haircut.price
+        super().save()
 
     class Meta:
         verbose_name = 'Работа'
